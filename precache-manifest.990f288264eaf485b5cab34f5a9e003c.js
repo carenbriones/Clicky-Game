1355f0a049803daaf3ef83371ee0924ef8ea3326
Caren B <carenpbriones@gmail.com>
2019-10-25 21:29:16 -0700

    Updates

---
+++ precache-manifest.990f288264eaf485b5cab34f5a9e003c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21346e320d4553c359b493d840d85fed",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "96bab10c5b87fd1eb417",
+    "url": "/Clicky-Game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e6effbca24685ced80aa",
+    "url": "/Clicky-Game/static/js/2.61f2fa50.chunk.js"
+  },
+  {
+    "revision": "96bab10c5b87fd1eb417",
+    "url": "/Clicky-Game/static/js/main.3fd2434b.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Clicky-Game/static/media/logo.25bf045c.svg"
+  }
+]);
