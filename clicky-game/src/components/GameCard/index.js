85f0cc704cc55ef65397f7f988302fb251c73524
Caren B <carenpbriones@gmail.com>
2019-10-27 23:24:21 -0700

    Added functionality for current score

--- clicky-game/src/components/GameCard/index.js
+++ clicky-game/src/components/GameCard/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 function GameCard(props) {
   return (
     <div className="img-container col-3">
-      <img alt={props.name} src={props.image} onClick={props.onClick}/>
+      <img alt={props.name} src={props.image} onClick={() => props.onClick(props.id)}/>
     </div>
   )
 }
--- clicky-game/src/pages/Game.js
+++ clicky-game/src/pages/Game.js
@@ -1,21 +1,39 @@
 import React, { Component } from "react";
 import GameCard from "../components/GameCard";
 import cards from "../cards.json";
+import "./style.css"
 
 class Game extends Component {
   state = {
     cards: cards,
     currentScore: 0,
     highestScore: 0,
-    clickedCards: []
+    clickedCards: [],
+    message: ""
   }
 
-  handleCardClick = () => {
-    this.setState(
-      {
-        cards: this.shuffle(this.state.cards)
-      });
-    console.log("clicked");
+  handleCardClick = (id) => {
+    let currentClickedCards = this.state.clickedCards;
+
+    // If the card has been clicked already, reset game
+    if (currentClickedCards.indexOf(id) > -1) {
+      this.setState({
+        cards: this.shuffle(this.state.cards),
+        currentScore: 0,
+        clickedCards: [],
+        message: "Incorrect!! Card has already been clicked!"
+      })
+      // If card hasn't been clicked, add to clickedCards
+    } else {
+      this.setState({
+        cards: this.shuffle(this.state.cards),
+        currentScore: this.state.currentScore + 1,
+        clickedCards: currentClickedCards.concat(id),
+        message: "Correct!"
+      })
+    }
+
+    
   }
 
   shuffle(cards) {
@@ -28,10 +46,22 @@ class Game extends Component {
 
   render() {
     return (
-      <div className="row">
-        {cards.map((card) => 
-          <GameCard {...card} key={card.id} onClick={this.handleCardClick}/>
-          )}
+      <div className="container">
+        <div className="row title">
+          <h1 className="col-12">Clicky Game</h1>
+        </div>
+        <div className="row game-container">
+          {cards.map((card) => 
+            <GameCard {...card} key={card.id} onClick={this.handleCardClick}/>
+            )}
+        </div>
+        <div className="row my-3 scores">
+          <h3 className="col-6">Current Score: {this.state.currentScore}</h3>
+          <h3 className="col-6">Highest Score: {this.state.highestScore}</h3>
+        </div>
+        <div className="row">
+          <h3>{this.state.message}</h3>
+        </div>
       </div>
     )
   }
